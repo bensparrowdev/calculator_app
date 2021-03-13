62251868647ffddd7b81b9f8109c6b86c4a07e1e
Ben Sparrow <bensparrowdev@gmail.com>
2021-03-13 14:14:18 +0000

    first init adding files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
+import { evaluate } from 'mathjs';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    screen: [],
+  };
+
+  inputHandle = (val) => {
+    let newScreen = this.state.screen
+    newScreen.push(val)
+    this.setState( {screen: newScreen} )
+  }
+
+  handleSubmit = () => {
+    let evalStr = this.state.screen.join("")
+    let output = evaluate(evalStr)
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Calculator App</h1>
+        <div className="screen">
+          <h2>{this.state.screen}</h2>
+        </div>
+        
+        <div className="buttons">
+          <CalcButton number="AC" clickFunction={this.inputHandle} />
+          <CalcButton number="*" clickFunction={this.inputHandle} />
+          <CalcButton number="/" clickFunction={this.inputHandle} />
+          <CalcButton number="-" clickFunction={this.inputHandle} />
+          <CalcButton number="+" clickFunction={this.inputHandle} />
+          <CalcButton number="=" clickFunction={this.inputHandle} />
+          <CalcButton number="." clickFunction={this.inputHandle} />
+          <CalcButton number="0" clickFunction={this.inputHandle} />
+          <CalcButton number="1" clickFunction={this.inputHandle} />
+          <CalcButton number="2" clickFunction={this.inputHandle} />
+          <CalcButton number="3" clickFunction={this.inputHandle} />
+          <CalcButton number="4" clickFunction={this.inputHandle} />
+          <CalcButton number="5" clickFunction={this.inputHandle} />
+          <CalcButton number="6" clickFunction={this.inputHandle} />
+          <CalcButton number="7" clickFunction={this.inputHandle} />
+          <CalcButton number="8" clickFunction={this.inputHandle} />
+          <CalcButton number="9" clickFunction={this.inputHandle} />
+          <button></button>
+        </div>
+      </div>
+    )
 }
+}
+
+const CalcButton = (props) => {
+  return (
+    <button onClick={() => props.clickFunction(props.number)}>
+      {props.number}
+    </button>
+  )
+} 
+
+
 
 export default App;
