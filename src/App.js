084ce2f6b0a10dbeb17a3691d5e84983935fad46
Ben Sparrow <bensparrowdev@gmail.com>
2021-03-15 13:49:44 +0000

    updating files, slight debug

--- src/App.js
+++ src/App.js
@@ -1,66 +1,81 @@
 import './App.css';
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { evaluate } from 'mathjs';
 
 class App extends Component {
   state = {
     screen: ["0"],
+    keys: [
+      { value: "AC", styling: "clear" },
+      { value: "÷", styling: "operator" },
+      { value: "7", styling: "num" },
+      { value: "8", styling: "num" },
+      { value: "9", styling: "num" },
+      { value: "×", styling: "operator" },
+      { value: "4", styling: "num" },
+      { value: "5", styling: "num" },
+      { value: "6", styling: "num" },
+      { value: "-", styling: "operator" },
+      { value: "1", styling: "num" },
+      { value: "2", styling: "num" },
+      { value: "3", styling: "num" },
+      { value: "+", styling: "operator" },
+      { value: "0", styling: "zero" },
+      { value: ".", styling: "num" },
+      { value: "=", styling: "operator" },
+    ],
   };
 
   inputHandle = (val) => {
-    let newScreen = [...this.state.screen, val]
-    if (newScreen[0] == "0"){
-      newScreen.shift()
+    if (val === "AC") {
+      this.setState({ screen: ["0"] });
     }
-    this.setState( {screen: newScreen} )
-  }
-
-  handleSubmit = () => {
-    let evalStr = this.state.screen.join("")
-    let output = evaluate(evalStr)
-    this.setState({screen: [output] })
-  }
-
-  handleClear = () => {
-    this.setState({ screen: ["0"]})
-  }
+    else if (val === "=") {
+      let evalStr = this.state.screen.join("");
+      let output = evaluate(evalStr);
+      this.setState({ screen: [output] });
+    }
+    else {
+      let newScreen = [...this.state.screen, val];
+      if (newScreen[0] === "0"){
+        newScreen.shift();
+      }
+      this.setState({ screen: [newScreen] });
+    }
+  };
 
   render() {
     return (
       <div>
         <h1>Calculator App</h1>
-        <div className="screen">
-          <h2>{this.state.screen}</h2>
-        </div>
-        
-        <div className="buttons">
-          <button onClick={this.handleClear}>AC</button>
-          <CalcButton number="*" clickFunction={this.inputHandle} />
-          <CalcButton number="/" clickFunction={this.inputHandle} />
-          <CalcButton number="-" clickFunction={this.inputHandle} />
-          <CalcButton number="+" clickFunction={this.inputHandle} />
-          <CalcButton number="." clickFunction={this.inputHandle} />
-          <CalcButton number="0" clickFunction={this.inputHandle} />
-          <CalcButton number="1" clickFunction={this.inputHandle} />
-          <CalcButton number="2" clickFunction={this.inputHandle} />
-          <CalcButton number="3" clickFunction={this.inputHandle} />
-          <CalcButton number="4" clickFunction={this.inputHandle} />
-          <CalcButton number="5" clickFunction={this.inputHandle} />
-          <CalcButton number="6" clickFunction={this.inputHandle} />
-          <CalcButton number="7" clickFunction={this.inputHandle} />
-          <CalcButton number="8" clickFunction={this.inputHandle} />
-          <CalcButton number="9" clickFunction={this.inputHandle} />
-
-          <button onClick={this.handleSubmit}>=</button>
+        <div className="calculator">
+          <div className="screen">
+            <h2>{this.state.screen}</h2>
+          </div>
+          
+          <div className="buttons">
+            {
+              this.state.keys.map( key => {
+                return (
+                <CalcButton 
+                  number={key.value} 
+                  styling={key.styling}
+                  clickFunction={this.inputHandle} />
+                );
+              })
+            }
+          </div>
         </div>
       </div>
     )
-}
+  }
 }
 
 const CalcButton = (props) => {
   return (
-    <button onClick={() => props.clickFunction(props.number)}>
+    <button 
+      className={`btn ${props.styling}`} 
+      onClick={() => props.clickFunction(props.number)}>
       {props.number}
     </button>
   )
